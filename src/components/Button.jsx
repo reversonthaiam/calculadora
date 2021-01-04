1f2eb324e0d464e54e7a34982ccc1e7a8208cdaf
reversonthaiam <70376757+reversonthaiam@users.noreply.github.com>
2021-01-04 16:03:04 -0300

    calculadora com layout inicial pronto

---
+++ src/components/Button.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "../components/Button.css";
+
+export default (props) => <button className="button">0</button>;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator></Calculator>
+  </div>
+    
+  ,
   document.getElementById('root')
 );
 
